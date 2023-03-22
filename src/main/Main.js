d42d013bd6576cdb6aa8c2c8cd3e9ba12b4250dc
= <kimadonghyon99@gmail.com>
2023-03-22 15:50:33 +0900

    menu story slide done

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,10 +1,19 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { API_URL } from '../config/apiurl';
+import useAsync from '../customHook/useAsync';
 import PostModal from '../page/PostModal';
 import FlowSection from './FlowSection';
 import Mainbottom from './Mainbottom';
 import Maintop from './Maintop';
 import MenuStory from './MenuStory';
 import SliderComp from './Slider';
+
+async function menuFetch(){
+    const response = await axios.get(`${API_URL}/getMenu`);
+    return response.data
+}
+
 
 const Mainpage = () => {
     const [Modal,setModal]=useState(false)
@@ -14,11 +23,16 @@ const Mainpage = () => {
     const onClosePost = ()=>{
         setModal(false)
     }
+    const {loading,error,data} = useAsync(()=>menuFetch(),[])
+    if (loading) return <div>로딩중</div>
+    if (error) return <div>에러발생</div>
+    if (!data) return null  
+
     return (
         <>
             {Modal && <PostModal onClose={onClosePost} data={Modal}/>}
             <Maintop/>
-            <MenuStory/>
+            <MenuStory data={data}/>
             {/* <Mainbottom/>
             <FlowSection imgname="lotuscookie.jpg" p={"바삭바삭"} header={"로터스쿠키"}/>
             <Mainbottom/>
--- src/main/MenuStory.js
+++ src/main/MenuStory.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
+import axios from 'axios';
+import { API_URL } from '../config/apiurl';
+import useAsync from '../customHook/useAsync';
 
 
 const Menustyle = styled.div`
@@ -18,7 +21,7 @@ const Menustyle = styled.div`
     }
     .menutitle{
         position:relative;
-        top:20px;
+        top:130px;
         color:#000;
         font-size:96px;
         font-weight:800;
@@ -29,6 +32,8 @@ const Menustyle = styled.div`
     }
     .tabs{
         width:320px;
+        position:relative;
+        top:300px;
         ul{
             display:flex;
             flex-direction:column;
@@ -41,6 +46,10 @@ const Menustyle = styled.div`
             margin:28px 0;
             font-size:24px;
             font-weight:900;
+            cursor: pointer;
+            &.active{
+                border-bottom:1px solid #333;
+            }
         }
     }
     .Menuslides{
@@ -49,67 +58,150 @@ const Menustyle = styled.div`
         width: 100%;
         min-width: 1320px;
         height: 516px;
-        top: 50%;
-        left: 50%;
+        top: 140%;
+        left: 35%;
         z-index: 0;
         margin: -200px 0 0 -90px;
+        .productname{
+            background-color:#000;
+            color:#fff;
+            text-align:center;
+            width:112px;
+            line-height:40px;
+            position:relative;
+            top: 78.3%;
+            left: 10%;
+            z-index: 2;
+        }
+        .slick-track{
+            display:flex;
+            align-items:flex-end;
+        }
         .box{
-            width:300;
-            height:300;
-            img{width:100%}
+            img{width:400px;height:400px}
         }
-        .slick-slide{
-            padding-right:40px;
+        .slick-slider {
+            margin:0 -15px;
         }
-        .slick-active{
-            width:300px !important;
+        .slick-slide {
+            padding:10px;
+            text-align:center;
+            margin:0 15px;
+            width: 400px;
+            height: 400px;
+        }        
+        .slick-prev {
+            left:110px;
+            z-index: 4;
+            top:96%;
+            width:40px;
+            height:40px;
+            background-color: #000;
+
+        }
+        .slick-next {
+            right: 78%;
+            top:96%;
+            width:40px;
+            height:40px;
+            background-color: #000;
+            z-index: 2;
+        }
+        .slick-prev:before{
+            width: 40px;
+            height: 40px;
+            content:"";
+            display:block;
+            line-height:1;
+            background:url('./imgs/ico_arrow_left.svg') 50% 50% no-repeat; 
+            color: #fff;        
+        }
+        .slick-next:before {
+            width: 40px;
+            height: 40px;
+            content:"";
+            display:block;
+            line-height:1;
+            background:url('./imgs/ico_arrow_right.svg') 50% 50% no-repeat; 
+            color: #fff;        
         }
 
     }
-
 `
-const settings = {
-    infinite:true,
-    speed:500,
-    slidesToShow:1,
-    slidesToScroll:1,
-    swipeToslide:true,
-    cssEase:"linear",
-    variableWidth: true
-}
 
-const MenuStory = () => {
+
+const MenuStory = ({data}) => {
+    const menutitle = useRef()
+    const bread = data.filter(e=>e.m_category === "bread")
+    const drink = data.filter(e=>e.m_category === "drink")
+    const giftset = data.filter(e=>e.m_category === "giftset")
+    const [category,setcategory] = useState(drink)
+    const onClick = (e)=>{
+        let list = document.querySelectorAll('#menuclick li')
+        console.log(e.target)
+        console.log(list)
+        list.forEach(li=>{
+            if(e.target === li){
+                if(e.target.className === 'Drinks'){
+                    setcategory(drink)
+                }else if(e.target.className === 'Bakery'){
+                    setcategory(bread)
+                }else{
+                    setcategory(giftset)
+                }
+                li.classList.add('active');
+            } else {
+                li.classList.remove('active')
+            }
+        })
+    }
+
+    const settings = {
+        className:'center',
+        speed:500,
+        slidesToShow:1,
+        slidesToScroll:1,
+        swipeToslide:true,
+        cssEase:"linear",
+        variableWidth: true,
+        afterChange:(e)=>menutitle.current.innerText =(category[e].m_name)
+    }
+    
     return (
         <Menustyle>
-            <div className="container">
+            <div className="container"> 
                 <h1 className='menutitle'>9899 MENUS<span>.</span></h1>
                 <div className='tabs'>
-                    <ul>
-                        <li>
-                            <strong className="main-subtitle">Coffee & drinks</strong>
+                    <ul id='menuclick' onClick={onClick}>
+                        <li className="Drinks active">
+                            Coffee & drinks
                         </li>                    
-                        <li>
-                            <strong className="main-subtitle">Bakery</strong>
+                        <li className="Bakery">
+                            Bakery
                         </li>                    
-                        <li>
-                            <strong className="main-subtitle">Gift set</strong>
+                        <li className="Giftset">
+                           Gift set
                         </li>                    
                     </ul>
                 </div>
                 <div className='Menuslides'>
+                    <div className='productname' ref={menutitle}>
+                        {category[0].m_name}
+                    </div>
                     <Slider {...settings}>
-                        <div className='box'  style={{ width: 200 }}>
+                        {category.map(menu=><div key={menu.m_number} className='box' style={{width:400,height:400}}><img src={`${API_URL}/upload/menu/${menu.m_img}`}/></div>)}
+                        {/* <div className='box' style={{width:400}}>
                             <img src='./imgs/Cookie.jpg'/>
                         </div>
-                        <div className='box'   style={{ width: 200 }}>
+                        <div className='box' style={{width:400}}>
                             <img src='./imgs/Cookie.jpg'/>
                         </div>
-                        <div className='box'   style={{ width: 200 }}>
+                        <div className='box' style={{width:400}}>
                             <img src='./imgs/Cookie.jpg'/>
                         </div>
-                        <div className='box'  style={{ width: 200 }}>
+                        <div className='box' style={{width:400}}>
                             <img src='./imgs/Cookie.jpg'/>
-                        </div>
+                        </div> */}
                     </Slider>
                 </div>
 
